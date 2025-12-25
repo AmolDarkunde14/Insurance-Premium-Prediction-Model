ff3525498f58bdf8c774c150cd9885813c358aa0
amol_darkunde <amoldarkunde014@gmail.com>
2025-12-25 23:13:13 +0530

    Prediction model API

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field, computed_field
 from typing import Literal, Annotated
 import pickle
 import pandas as pd
 
-# load the ml model file
+# import the ml model
 with open('model.pkl', 'rb') as f:
     model = pickle.load(f)
 
@@ -20,24 +21,23 @@ tier_2_cities = [
     "Kolhapur", "Bilaspur", "Jalandhar", "Noida", "Guntur", "Asansol", "Siliguri"
 ]
 
-# pydantic model to validate the user input
-
+# pydantic model to validate incoming data
 class UserInput(BaseModel):
 
     age: Annotated[int, Field(..., gt=0, lt=120, description='Age of the user')]
     weight: Annotated[float, Field(..., gt=0, description='Weight of the user')]
-    height: Annotated[float, Field(..., gt=0, lt=2.5, description='Hirght of the user')]
-    income_lpa: Annotated[float, Field(..., gt=0, description='Annual Income of the user')]
+    height: Annotated[float, Field(..., gt=0, lt=2.5, description='Height of the user')]
+    income_lpa: Annotated[float, Field(..., gt=0, description='Annual salary of the user in lpa')]
     smoker: Annotated[bool, Field(..., description='Is user a smoker')]
-    city: Annotated[str, Field(..., description='The city that user belongs to')]
+    city: Annotated[str, Field(..., description='The city that the user belongs to')]
     occupation: Annotated[Literal['retired', 'freelancer', 'student', 'government_job',
        'business_owner', 'unemployed', 'private_job'], Field(..., description='Occupation of the user')]
-
+    
     @computed_field
     @property
     def bmi(self) -> float:
         return self.weight/(self.height**2)
-
+    
     @computed_field
     @property
     def lifestyle_risk(self) -> str:
@@ -47,7 +47,7 @@ class UserInput(BaseModel):
             return "medium"
         else:
             return "low"
-    
+        
     @computed_field
     @property
     def age_group(self) -> str:
@@ -68,3 +68,23 @@ class UserInput(BaseModel):
             return 2
         else:
             return 3
+
+@app.post('/predict')
+def predict_premium(data: UserInput):
+
+    input_df = pd.DataFrame([{
+        'bmi': data.bmi,
+        'age_group': data.age_group,
+        'lifestyle_risk': data.lifestyle_risk,
+        'city_tier': data.city_tier,
+        'income_lpa': data.income_lpa,
+        'occupation': data.occupation
+    }])
+
+    prediction = model.predict(input_df)[0]
+
+    return JSONResponse(status_code=200, content={'predicted_category': prediction})
+
+
+
+
